c8a9248f6cda4a466ad846762b8c788fcb035fe2
d-zaloziecki <pnides20@gmail.com>
2018-10-09 13:01:51 +0300

    added rating diet and alergans to schemas

--- models/commentModel.js
+++ models/commentModel.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 let commentSchma = new Schema({
     userName:String,
     text: String,
+    rating: { type: Number, min: 0, max: 5 },
     recipe: {type: Schema.Types.ObjectId, ref: 'recipe'}
 });
 
